f36211bf6c8233d4fd9adcdaffcf2076b0faebc9
YI RAO <yi.rao@sh.shin-china.com>
2025-12-19 10:00:29 +0000

    VN 20251219 PP-047 Model Point Report

---
+++ pp-s4apps01/zmodelpoint/webapp/controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("pp.zmodelpoint.controller.App", {
+        onInit: function () {
+            
+        },
+    });
+});
+
---
+++ pp-s4apps01/zmodelpoint/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["pp/zmodelpoint/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
