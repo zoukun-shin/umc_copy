15633b140cee7462ecd22c906db41f8777cc8bc1
ZHAO WANG <zhao.wang@sh.shin-china.com>
2026-01-06 07:07:42 +0000

    VN 20260106 FICO-029 Display Line Items in General Ledger AUTH and I18n Add

--- fico-s4apps01/generalledger/webapp/controller/Main.controller.js
+++ fico-s4apps01/generalledger/webapp/controller/Main.controller.js
@@ -7,6 +7,7 @@ sap.ui.define([
 	"sap/m/BusyDialog",
 	"sap/ui/export/Spreadsheet",
 	"./messages",
+    "sap/ui/core/Messaging",
 ], function (
 	Base,
 	formatter,
@@ -15,7 +16,8 @@ sap.ui.define([
 	FilterOperator,
 	BusyDialog,
 	Spreadsheet,
-	messages
+	messages,
+    Messaging
 ) {
 	"use strict";
 
@@ -28,15 +30,73 @@ sap.ui.define([
 			this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 			this._BusyDialog = new BusyDialog();
 			var oRouter = this.getRouter();
-			oRouter.getRoute("Main").attachMatched(this._onRouteMatched, this);
+			oRouter.getRoute("Main").attachMatched(this._initialize, this);
 		},
 		_onRouteMatched: function (oEvent) {
 			this.getView().getModel().resetChanges();
 			this._UserInfo = sap.ushell.Container.getService("UserInfo");
 		},
 
-		onBeforeRebindTable: function (oEvent) {
-		}
-		
+		_initialize: function (oEvent) {
+            Messaging.removeAllMessages();
+            this._authorityCheck();
+        },
+
+		_authorityCheck: function () {
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "generalledger-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.getView().destroy();
+                    this.oErrorMessageDialog.open();
+                }
+                this.getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "zngmanangement-View"),
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.getView().destroy();
+                this.oErrorMessageDialog.open();
+            }.bind(this));
+        },
+
 	});
 });
