852c7beccadb7ce1d08eb2ec4ba539f9df9a68cf
KUN ZOU <kun.zou@sh.shin-china.com>
2026-01-12 14:44:28 +0000

    TH/VN 20260112 PP-008 ECN Report  add print button permission

--- pp-s4apps01/zecn/webapp/ext/controller/ListReportExt.js
+++ pp-s4apps01/zecn/webapp/ext/controller/ListReportExt.js
@@ -24,58 +24,60 @@ sap.ui.define([
         },
 
         _getAuthorityData: function (oAuthorityModel, oLocalModel, oI18nModel, oViews) {
-            // var sUser = _UserInfo.getFullName() === undefined ? "" : _UserInfo.getFullName();
-            // var sEmail = _UserInfo.getEmail() === undefined ? "" : _UserInfo.getEmail();
-            // var oContextBinding = oAuthorityModel.bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
-            //     "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
-            // });
-            // oContextBinding.requestObject().then(function (context) {
-            //     var aAccessBtns = [],
-            //         aAllAccessBtns = [];
-            //     if (context._AssignRole && context._AssignRole.length > 0) {
-            //         context._AssignRole.forEach(role => {
-            //             aAccessBtns.push(role._UserRoleAccessBtn);
-            //         });
-            //         aAllAccessBtns = aAccessBtns.flat();
-            //     }
-            //     if (!aAllAccessBtns.some(btn => btn.AccessId === "zecn-View")) {
-            //         if (!this.oErrorMessageDialog) {
-            //             this.oErrorMessageDialog = new sap.m.Dialog({
-            //                 type: sap.m.DialogType.Message,
-            //                 state: "Error",
-            //                 content: new sap.m.Text({
-            //                     text: oI18nModel.getResourceBundle().getText("noAuthorityView", [sUser])
-            //                 })
-            //             });
-            //         }
-            //         oViews.destroy();
-            //         this.oErrorMessageDialog.open();
-            //     }
-            //     oLocalModel.setProperty("/authorityCheck", {
-            //         button: {
-            //             View: aAllAccessBtns.some(btn => btn.AccessId === "zecn-View")
-            //         },
-            //         data: {
-            //             PlantSet: context._AssignPlant,
-            //             CompanySet: context._AssignCompany,
-            //             SalesOrgSet: context._AssignSalesOrg,
-            //             PurchOrgSet: context._AssignPurchOrg,
-            //             RoleSet: context._AssignRole
-            //         }
-            //     });
-            // }.bind(this), function (oError) {
-            //     if (!this.oErrorMessageDialog) {
-            //         this.oErrorMessageDialog = new sap.m.Dialog({
-            //             type: sap.m.DialogType.Message,
-            //             state: "Error",
-            //             content: new sap.m.Text({
-            //                 text: oI18nModel.getResourceBundle().getText("getAuthorityFailed")
-            //             })
-            //         });
-            //     }
-            //     oViews.destroy();
-            //     this.oErrorMessageDialog.open();
-            // }.bind(this));
+            var sUser = _UserInfo.getFullName() === undefined ? "" : _UserInfo.getFullName();
+            var sEmail = _UserInfo.getEmail() === undefined ? "" : _UserInfo.getEmail();
+            // sEmail = "xinlei.xu@sh.shin-china.com";
+            var oContextBinding = oAuthorityModel.bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "zecn-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: oI18nModel.getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    oViews.destroy();
+                    this.oErrorMessageDialog.open();
+                }
+                oLocalModel.setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "zecn-View"),
+                        Print: aAllAccessBtns.some(btn => btn.AccessId === "zecn-Print")
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: oI18nModel.getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                oViews.destroy();
+                this.oErrorMessageDialog.open();
+            }.bind(this));
         },
 
         printAction: function (oEvent) {
