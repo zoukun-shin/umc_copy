e8e17353c823b44b9bc4ea3b3bfb9c95cdff01a2
KUN ZOU <kun.zou@sh.shin-china.com>
2026-01-12 14:50:43 +0000

    TH/VN 20260112 PP-041 Capacity Load Analysis Report add permission

--- pp-s4apps01/capacityload/webapp/controller/Main.controller.js
+++ pp-s4apps01/capacityload/webapp/controller/Main.controller.js
@@ -1,20 +1,78 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
+    "./BaseController",
     "../model/formatter",
 	"./messages",
 	"sap/ui/core/Fragment",
 	"sap/m/Dialog",
 	"sap/ui/model/Filter",
-], (Controller,formatter,messages,Fragment,Dialog,Filter) => {
+], (BaseController,formatter,messages,Fragment,Dialog,Filter) => {
     "use strict";
 
-    return Controller.extend("pp.capacityload.controller.Main", {
+    return BaseController.extend("pp.capacityload.controller.Main", {
         formatter: formatter,
         onInit() {
             this._LocalData = this.getOwnerComponent().getModel("local");
 			this._oDataModel = this.getOwnerComponent().getModel();
 			this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 			this._BusyDialog = new sap.m.BusyDialog();
+
+			this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            this.getRouter().getRoute("RouteMain").attachMatched(this._initialize, this);
+        },
+		_initialize: function () {
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            // var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+			sEmail = "xinlei.xu@sh.shin-china.com";
+            var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "capacityload-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.getView().destroy();
+                    this.oErrorMessageDialog.open();
+                }
+                this.getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "capacityload-View"),
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.getView().destroy();
+                this.oErrorMessageDialog.open();
+            }.bind(this));
         },
         onBeforeRebindTable: function (oEvent) {
             var mBindingParams = oEvent.getParameter("bindingParams");
@@ -36,7 +94,7 @@ sap.ui.define([
             }
 			oNewFilter = new Filter({
 				filters:aNewFilter,
-				and:false
+				and:true
 			});
 			if (aNewFilter.length > 0) {
 				oFilter.push(oNewFilter);
