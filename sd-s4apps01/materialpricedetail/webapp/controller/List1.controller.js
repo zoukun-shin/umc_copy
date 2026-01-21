87a48574df89d45057517f3a95ada942074d6ada
QIANGWEI WANG <qiangwei.wang@sh.shin-china.com>
2026-01-21 06:33:53 +0000

    VN 20260121 SD-046 Material Price Detail CR#5606

---
+++ sd-s4apps01/materialpricedetail/webapp/controller/List1.controller.js
@@ -0,0 +1,81 @@
+sap.ui.define([
+    "./Base",
+    "../model/formatter",
+    "sap/m/BusyDialog",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/core/Fragment",
+    "sap/ui/export/Spreadsheet",
+], (Base, formatter, BusyDialog, MessageBox, MessageToast, Filter, FilterOperator, Fragment, Spreadsheet) => {
+    "use strict";
+
+    return Base.extend("sd.materialpricedetail.controller.List1", {
+        formatter: formatter,
+        onInit() {
+            var that = this;
+            this._LocalData = this.getOwnerComponent().getModel("local");
+            this._oDataModel = this.getOwnerComponent().getModel();
+            this._oDataModel.setRefreshAfterChange(false);
+            this._BusyDialog = new BusyDialog();
+            if (sap.ushell && sap.ushell.Container) {
+                this._UserInfo = sap.ushell.Container.getService("UserInfo").getUser();
+            };
+        },
+
+        onBeforeRebindTable: function (oEvent) {
+            let afilters = oEvent.getParameters().bindingParams.filters;
+            var oGjahr = new Date(this.byId("idGjahr1").getValue());
+            afilters.push(new sap.ui.model.Filter( "Gjahr", "EQ", oGjahr.getFullYear()));
+            var oMonth = this.byId("idMonat1");
+            if (oMonth) {
+                var aMonth = oMonth.getSelectedKeys();
+                aMonth.forEach((e) => {
+                    afilters.push(new sap.ui.model.Filter( "Monat", "EQ", e));
+                })
+            };
+
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            afilters.push(new sap.ui.model.Filter("UserEmail", "EQ", sEmail));
+            //Tab1
+            afilters.push(new sap.ui.model.Filter("Type", "EQ", "1"));
+        },
+
+        onBeforeExport: function (oEvent) {
+            var mExcelSettings = oEvent.getParameter("exportSettings");
+            var sFileName = this.getModel("i18n").getResourceBundle().getText("title1");
+            this._exportExcel(mExcelSettings, sFileName);
+        },
+
+        _exportExcel: function (mExcelSettings, sFileName) {
+            mExcelSettings.workbook.columns.forEach(function (oColumn) {
+                switch (oColumn.property) {
+                    //  Date
+                    case "PostingDate":
+                        oColumn.type = sap.ui.export.EdmType.Date;
+                        break;
+                    case "Quantity":
+                        oColumn.type = sap.ui.export.EdmType.Number;
+                        oColumn.delimiter = true;
+                        oColumn.scale = 3;
+                        oColumn.textAlign = "End";
+                        break;
+                    case "UnitPrice":
+                        oColumn.type = sap.ui.export.EdmType.Number;
+                        oColumn.delimiter = true;
+                        oColumn.scale = 5;
+                        oColumn.textAlign = "End";
+                        break;
+                    case "SummaryQuantity":
+                        oColumn.type = sap.ui.export.EdmType.Number;
+                        oColumn.delimiter = true;
+                        oColumn.scale = 3;
+                        oColumn.textAlign = "End";
+                        break;
+                }
+            });
+            mExcelSettings.fileName = sFileName + "_" + this.getCurrentDateTime();
+        }
+    });
+});
---
+++ sd-s4apps01/materialpricedetail/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sd/materialpricedetail/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
