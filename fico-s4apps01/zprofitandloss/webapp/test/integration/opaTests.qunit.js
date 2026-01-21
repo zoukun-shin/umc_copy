33c22691f021159a5845ee720875f7ab911cd07d
YI RAO <yi.rao@sh.shin-china.com>
2026-01-21 12:50:15 +0000

    VN 20260121 FICO-030 Profit and Loss Statement for Functional Currency

---
+++ fico-s4apps01/zprofitandloss/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["fico/zprofitandloss/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ fico-s4apps01/zprofitandloss/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"fico/zprofitandloss/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
