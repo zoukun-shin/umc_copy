f36bda6e4742093180a7c8380e35f3a153e50c03
KUN ZOU <kun.zou@sh.shin-china.com>
2026-02-04 04:00:00 +0000

    TH/VN 20260204 PP-008 ECN Report  fix bug

--- pp-s4apps01/zecn/webapp/ext/controller/ListReportExt.js
+++ pp-s4apps01/zecn/webapp/ext/controller/ListReportExt.js
@@ -26,7 +26,7 @@ sap.ui.define([
         _getAuthorityData: function (oAuthorityModel, oLocalModel, oI18nModel, oViews) {
             var sUser = _UserInfo.getFullName() === undefined ? "" : _UserInfo.getFullName();
             var sEmail = _UserInfo.getEmail() === undefined ? "" : _UserInfo.getEmail();
-            sEmail = "xinlei.xu@sh.shin-china.com";
+            // sEmail = "xinlei.xu@sh.shin-china.com";
             var oContextBinding = oAuthorityModel.bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
                 "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
             });
