0c77361e4ec20b9e38537b78208480e7e47d0b72
XINLEI XU <xinlei.xu@sh.shin-china.com>
2026-05-20 09:59:26 +0000

    JP 20260520 BTPの権限ロール割当の一括エクスポート機能追加 AMO #5876

--- bc-s4apps01/zauthorityrolelist/webapp/ext/controller/ListReportExt.js
+++ bc-s4apps01/zauthorityrolelist/webapp/ext/controller/ListReportExt.js
@@ -5,9 +5,12 @@ sap.ui.define([
 ], function (BusyDialog, MessageBox, MessageToast) {
     'use strict';
 
+    var _myFunction, _myBusyDialog;
     return {
 
         getAuthorityData: function (oModels, oViews) {
+            _myFunction = sap.ui.require("bc/zauthorityrolelist/ext/controller/ListReportExt");
+            _myBusyDialog = new BusyDialog();
             var oAuthorityModel = oModels.Authority;
             var oLocalModel = oModels.local;
             var oI18nModel = oModels.i18n;
@@ -64,6 +67,73 @@ sap.ui.define([
                 oViews.destroy();
                 this.oErrorMessageDialog.open();
             }.bind(this));
-        }
+        },
+
+        // ADD BEGIN BY XINLEI XU 2026/04/13 AMO#5876
+        onDownload: function (oEvent) {
+            var that = this;
+            var aItems = [];
+            var aContexts = this._controller.extensionAPI.getSelectedContexts();
+            aContexts.forEach(element => {
+                let aSplitArray = element.getPath().split("'");
+                aItems.push({
+                    TimezoneOffset: -new Date().getTimezoneOffset() / 60, // Timezone offset in hours
+                    RoleId: decodeURIComponent(aSplitArray[1])
+                });
+            });
+            if (aItems.length === 0) {
+                aItems.push({
+                    TimezoneOffset: -new Date().getTimezoneOffset() / 60, // Timezone offset in hours
+                    RoleId: ""
+                });
+            }
+            _myFunction._callOData("EXPORT", aItems, that);
+        },
+
+        _callOData: function (bEvent, oRequestData, that) {
+            var aPromise = [];
+            aPromise.push(_myFunction._callODataAction(bEvent, oRequestData, that));
+            try {
+                _myBusyDialog.open();
+                Promise.all(aPromise).then((aContext) => {
+                    _myBusyDialog.close();
+                    var aDownloadRecords = [];
+                    for (const activeContext of aContext) {
+                        var boundContext = activeContext.getBoundContext();
+                        var object = boundContext.getObject();
+                        aDownloadRecords.push({
+                            RecordUUID: object.RecordUUID
+                        });
+                    }
+                    aDownloadRecords.forEach(element => {
+                        var sPath = "PrintRecord(RecordUUID=" + element.RecordUUID + ",IsActiveEntity=true)";
+                        var sURL = that.getModel("Print").getServiceUrl() + sPath + '/PDFContent';
+                        sap.m.URLHelper.redirect(sURL, true);
+                    });
+                }).catch((error) => {
+                    MessageBox.error(error);
+                }).finally(() => {
+                    _myBusyDialog.close();
+                });
+            } catch (error) {
+                MessageBox.error(error);
+                _myBusyDialog.close();
+            }
+        },
+
+        _callODataAction: function (bEvent, aRequestData, that) {
+            return new Promise((resolve, reject) => {
+                var processLogic = that.getModel().bindContext("/Role/com.sap.gateway.srvd.zui_permission_access_o4.v0001.processLogic(...)");
+                processLogic.setParameter("Event", bEvent);
+                processLogic.setParameter("Zzkey", JSON.stringify(aRequestData));
+                processLogic.setParameter("RecordUUID", "");
+                processLogic.execute("$auto", false, null, /*bReplaceWithRVC*/false).then(() => {
+                    resolve(processLogic);
+                }).catch((error) => {
+                    reject(error);
+                });
+            });
+        },
+        // ADD END BY XINLEI XU 2026/04/13 AMO#5876
     };
 });
