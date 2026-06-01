365a4f20e4f4ee814ed2941f7b6d786646f3ccc9
ZHAO WANG <zhao.wang@sh.shin-china.com>
2026-06-01 00:43:39 +0000

    CN 20260601 PP-064 生产版本报表 需求 No.42 & 43

---
+++ pp-s4apps01/zproductionversionreport/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["pp/zproductionversionreport/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ pp-s4apps01/zproductionversionreport/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"pp/zproductionversionreport/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
