c632ede421a59c34faf67e9e8dfbefbb6a9606ed
ZHAO WANG <zhao.wang@sh.shin-china.com>
2026-06-26 10:13:54 +0000

    VN 20260626 FICO-033 Vendor line item CM#5535

--- fico-s4apps01/zvendorlineitemreport/webapp/controller/Main.controller.js
+++ fico-s4apps01/zvendorlineitemreport/webapp/controller/Main.controller.js
@@ -1,49 +1,31 @@
 sap.ui.define([
-	"./Base",
-	"../model/formatter",
-	"sap/m/MessageBox",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-	"sap/m/BusyDialog",
-	"sap/ui/export/Spreadsheet",
-	"./messages",
-    "sap/ui/core/Messaging",
-], function (
-	Base,
-	formatter,
-	MessageBox,
-	Filter,
-	FilterOperator,
-	BusyDialog,
-	Spreadsheet,
-	messages,
-    Messaging
-) {
-	"use strict";
+    "./Base",
+    "../model/formatter",
+    "sap/m/BusyDialog",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast",
+    "sap/m/DynamicDateUtil",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/core/Fragment",
+    "sap/ui/export/Spreadsheet",
+], (Base, formatter, BusyDialog, MessageBox, MessageToast, DynamicDateUtil, Filter, FilterOperator, Fragment, Spreadsheet) => {
+    "use strict";
 
-	return Base.extend("fico.zvendorlineitemreport.controller.Main", {
+    return Base.extend("fico.zvendorlineitemreport.controller.Main", {
 		formatter: formatter,
-		messages: messages,
-		onInit: function () {
-			this._LocalData = this.getOwnerComponent().getModel("local");
-			this._oDataModel = this.getOwnerComponent().getModel();
-			this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-			this._BusyDialog = new BusyDialog();
-			var oRouter = this.getRouter();
-			oRouter.getRoute("Main").attachMatched(this._initialize, this);
-		},
-		_onRouteMatched: function (oEvent) {
-			this.getView().getModel().resetChanges();
-			this._UserInfo = sap.ushell.Container.getService("UserInfo");
-		},
-
-		_initialize: function (oEvent) {
-            Messaging.removeAllMessages();
-            this._authorityCheck();
+		onInit() {
+            this._LocalData = this.getOwnerComponent().getModel("local");
+            this._oDataModel = this.getOwnerComponent().getModel();
+            this._oDataModel.setRefreshAfterChange(false);
+            this._BusyDialog = new BusyDialog();
+            if (sap.ushell && sap.ushell.Container) {
+                this._UserInfo = sap.ushell.Container.getService("UserInfo").getUser();
+            };
+            this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
         },
 
-		_authorityCheck: function () {
-            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+		_initialize: function (oEvent) {
             var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
             var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
             var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
