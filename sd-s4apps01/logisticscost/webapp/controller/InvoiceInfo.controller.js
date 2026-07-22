e6ef08b29111e31b1d59445ede9f6965cb77c770
zoukun-shin <kun.zou@sh.shin-china.com>
2026-07-22 17:33:17 +0800

    CN 20260722 SD-049 物流费用分析报表 GAP No.37

---
+++ sd-s4apps01/logisticscost/webapp/controller/InvoiceInfo.controller.js
@@ -0,0 +1,34 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "../model/formatter"
+], function(Controller, Filter, formatter) {
+    "use strict";
+    return Controller.extend("sd.logisticscost.controller.InvoiceInfo", {
+
+        formatter: formatter,
+
+        onInit() {},
+
+        onBeforeRebindTable: function(oEvent) {
+            var oBindingParams = oEvent.getParameter("bindingParams");
+            var oFilter = oBindingParams.filters;
+            var aNewFilter = [];
+
+            var oDateRange = this.byId("idDRPlannedGoodsIssueDate");
+            var oStartDate = oDateRange.getDateValue();
+            var oEndDate = oDateRange.getSecondDateValue();
+            if (oStartDate && oEndDate) {
+                aNewFilter.push(new Filter(
+                    "PlannedGoodsIssueDate", "BT",
+                    formatter.odataDate(oStartDate),
+                    formatter.odataDate(oEndDate)
+                ));
+            }
+
+            if (aNewFilter.length > 0) {
+                oFilter.push(new Filter({ filters: aNewFilter, and: true }));
+            }
+        }
+    });
+});
