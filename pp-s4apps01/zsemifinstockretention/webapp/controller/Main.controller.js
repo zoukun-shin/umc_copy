4c013b769c554fa71af66973edcfc667c422a749
siyun <siyun.yao@sh.shin-china.com>
2026-08-04 08:19:02 +0000

    CN 20260804 PP-070 半制品在库保有日报表 需求No.184

--- pp-s4apps01/zsemifinstockretention/webapp/controller/Main.controller.js
+++ pp-s4apps01/zsemifinstockretention/webapp/controller/Main.controller.js
@@ -22,7 +22,7 @@ sap.ui.define([
             _initialize: function () {
                 this._UserInfo = sap.ushell.Container.getService("UserInfo");
                 var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
-                var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+                var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail(); 
                 var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
                     "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
                 });
@@ -106,28 +106,100 @@ sap.ui.define([
                     });
                 }
             },
-onBeforeRebindTable: function (oEvent) {
-    var mBindingParams = oEvent.getParameter("bindingParams");
+            onBeforeRebindTable: function (oEvent) {
+                var mBindingParams = oEvent.getParameter("bindingParams");
 
-    // customControl 的日期值不会自动进查询，手动取出并加入 filter
-    var oDateRange = this.byId("dateRangeStartDate");
-    var dLow  = oDateRange.getDateValue();        // 起始日
-    var dHigh = oDateRange.getSecondDateValue();  // 结束日
+                // 工厂权限校验，不通过则不查询
+                if (!this._checkPlantAuthority()) {
+                    mBindingParams.preventTableBind = true;
+                    return;
+                }
 
-    if (dLow && dHigh) {
-        mBindingParams.filters.push(new Filter({
-            path: "MfgOrderPlannedStartDate",
-            operator: FilterOperator.BT,
-            value1: dLow,
-            value2: dHigh
-        }));
-    } else if (dLow) {
-        mBindingParams.filters.push(new Filter({
-            path: "MfgOrderPlannedStartDate",
-            operator: FilterOperator.GE,
-            value1: dLow
-        }));
-    }
-}
+                // customControl 的日期值不会自动进查询，手动取出并加入 filter
+                var oDateRange = this.byId("dateRangeStartDate");
+                var dLow = oDateRange.getDateValue();        // 起始日
+                var dHigh = oDateRange.getSecondDateValue();  // 结束日
+
+                if (dLow && dHigh) {
+                    mBindingParams.filters.push(new Filter({
+                        path: "MfgOrderPlannedStartDate",
+                        operator: FilterOperator.BT,
+                        value1: dLow,
+                        value2: dHigh
+                    }));
+                } else if (dLow) {
+                    mBindingParams.filters.push(new Filter({
+                        path: "MfgOrderPlannedStartDate",
+                        operator: FilterOperator.GE,
+                        value1: dLow
+                    }));
+                }
+            },
+
+            // 校验检索条件里的工厂是否都在用户权限内
+            // 返回 false 表示不通过，已弹出错误框
+            _checkPlantAuthority: function () {
+                var aAuthorityPlantSet = this.getModel("local").getProperty("/authorityCheck/data/PlantSet");
+                if (!aAuthorityPlantSet) {
+                    // 权限数据还没取回来，先不拦，_initialize 里失败会另行弹窗
+                    return true;
+                }
+
+                var aPlants = this._getFilterPlants();
+                if (aPlants.length === 0) {
+                    return true;
+                }
+
+                var oBundle = this.getModel("i18n").getResourceBundle();
+                for (var i = 0; i < aPlants.length; i++) {
+                    var sPlant = aPlants[i];
+                    var bHasAuthority = aAuthorityPlantSet.some(function (oAuth) {
+                        return oAuth.Plant === sPlant;
+                    });
+                    if (!bHasAuthority) {
+                        MessageBox.error(oBundle.getText("noAuthorityPlant", [sPlant]));
+                        return false;
+                    }
+                }
+                return true;
+            },
+
+            // 取检索条件里用户输入的所有工厂，值帮助选择项和手工区间都要取
+            _getFilterPlants: function () {
+                var oSFB = this.byId("SFBSemiFinStockRetention");
+                if (!oSFB) {
+                    return [];
+                }
+
+                var oPlantData = (oSFB.getFilterData() || {}).Plant;
+                if (!oPlantData) {
+                    return [];
+                }
+                if (typeof oPlantData === "string") {
+                    return [oPlantData];
+                }
+
+                var aPlants = [];
+                (oPlantData.items || []).forEach(function (oItem) {
+                    if (oItem.key) {
+                        aPlants.push(oItem.key);
+                    }
+                });
+                (oPlantData.ranges || []).forEach(function (oRange) {
+                    if (oRange.value1) {
+                        aPlants.push(oRange.value1);
+                    }
+                    if (oRange.value2) {
+                        aPlants.push(oRange.value2);
+                    }
+                });
+                if (oPlantData.value) {
+                    aPlants.push(oPlantData.value);
+                }
+
+                return aPlants.filter(function (sPlant, iIndex) {
+                    return aPlants.indexOf(sPlant) === iIndex;
+                });
+            }
         });
     });
