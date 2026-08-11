aadb5718e219c86fa3bf44fd39c4d12ce2d1533b
YI RAO <yi.rao@sh.shin-china.com>
2026-08-11 05:35:19 +0000

    CN 20260811 PP-055 BOM版本查询 改修 需求 No.65

--- pp-s4apps01/zbomversion/webapp/controller/App.controller.js
+++ pp-s4apps01/zbomversion/webapp/controller/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], (Controller) => {
     "use strict";
 
-    return Controller.extend("pp.zbomversion.controller.Main", {
+    return Controller.extend("pp.zbomversion.controller.App", {
         onInit() {
         }
     });
