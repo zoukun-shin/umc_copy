9ccb566af89704606387f7a6dbb6bba44f4a4edc
XINLEI XU <xinlei.xu@sh.shin-china.com>
2026-08-29 06:30:29 +0000

    CN 20260829 MM-063 供应商主数据查询报表 需求 No.198

---
+++ mm-s4apps01/zsuppliermasterreport/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("mm.zsuppliermasterreport.controller.App", {
+        onInit() {
+        }
+    });
+});
---
+++ mm-s4apps01/zsuppliermasterreport/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["mm/zsuppliermasterreport/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
