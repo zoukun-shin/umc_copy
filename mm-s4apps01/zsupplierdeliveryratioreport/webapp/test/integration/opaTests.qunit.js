9073b4068443e6210a6e74b8a2941b6717652e3e
XINLEI XU <xinlei.xu@sh.shin-china.com>
2026-08-29 08:52:44 +0000

    CN 20260829 MM-078 供应商交货达成率报表 需求 No.201

---
+++ mm-s4apps01/zsupplierdeliveryratioreport/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["mm/zsupplierdeliveryratioreport/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ mm-s4apps01/zsupplierdeliveryratioreport/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"mm/zsupplierdeliveryratioreport/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
