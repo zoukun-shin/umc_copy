d25fb55956424e34315f5da35a47cd613f028cf2
KUN ZOU <kun.zou@sh.shin-china.com>
2026-09-03 07:58:28 +0000

    private CN 20260903 PP-057 ECO作成检查 需求 No.112

---
+++ pp-s4apps01/ecocheck/webapp/Component.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "pp/ecocheck/model/models"
+], (UIComponent, models) => {
+    "use strict";
+
+    return UIComponent.extend("pp.ecocheck.Component", {
+        metadata: {
+            manifest: "json",
+            interfaces: [
+                "sap.ui.core.IAsyncContentCreation"
+            ]
+        },
+
+        init() {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
+
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+
+            // enable routing
+            this.getRouter().initialize();
+        }
+    });
+});
---
+++ pp-s4apps01/ecocheck/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"pp/ecocheck/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
